ee2476c54032cc7abfc9dafeb438b0c9378f82bc
filamin <filamin@ya.ru>
2020-12-08 00:55:16 +0300

    trying fix bug with npm
    npm на гите показал
    npm ERR! missing script: lint
    пытаюсь фиксить

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/essential"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["vue"],
+  rules: {},
 };
+s
